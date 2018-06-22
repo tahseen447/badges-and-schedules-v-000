0db01ddbc8bfcc2ebaa24e5bdc27e6ecf422fc76
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-22 19:57:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,10 @@ end
 
 def assign_rooms(attendees)
   counter = 1
-  attendees.collect {|attendee| "Hello, #{attendee}! You'll be assigned to room #{counter}!" counter+=1}
+  attendees.collect do |attendee|
+    "Hello, #{attendee}! You'll be assigned to room #{counter}!"
+    counter+=1
+  end
 end
 
 def printer(attendees)
